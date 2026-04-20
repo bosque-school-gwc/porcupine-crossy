68b1a6bc53665fe0ff42f6f5f12849fed04dbfe1
bareiss-bosque <tassos.bareiss@bosqueschool.org>
2026-04-20 15:50:01 -0600

    Add wilbert and collisions

--- sketch.js
+++ sketch.js
@@ -1,27 +1,50 @@
 CANVAS_WIDTH = 800
 CANVAS_HEIGHT = 600
 ROW_HEIGHT = 50
-NEW_ROW_INTERVAL = 200
+NEW_ROW_INTERVAL = 100
+CAR_WIDTH = 100
 
 // A row can be a road or a median
 let rows = [];
+const wilbert = {
+    x: CANVAS_WIDTH / 2,
+    y: CANVAS_HEIGHT / 2 - ROW_HEIGHT
+}
 
 function setup() {
     createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
-    background("green");
+    
 }
 
+let gameOver = false;
+
 function draw() {
+    background("green");
+
+    if (gameOver) {
+        background("red")
+        textSize(32);
+        text("Game over", 350, 300)
+        return
+    }
+
     rows.forEach(row => {
         drawRow(row)
     })
-    
-    if (frameCount % NEW_ROW_INTERVAL == 0) { 
-        // At a steady rate, generate new rows at the top of the canvas
-        rows.forEach(row => {
-            row.y = row.y + ROW_HEIGHT
-        })
 
+    moveWilbert()
+    drawWilbert()
+    
+    rows.forEach(row => {
+        row.y = row.y + 1
+    })
+
+    checkGameOver()
+
+    wilbert.y += 1
+  
+
+    if (frameCount % ROW_HEIGHT == 0) { 
         // Randomly decide to generate a road with cars or a grassy median
         if (random(0,1) < .6) {
             rows.push(newRoad())
@@ -34,6 +57,57 @@ function draw() {
             return row.y < CANVAS_HEIGHT
         })
     }
+}
+
+function checkGameOver() {
+    if (wilbert.y > CANVAS_HEIGHT - 50 || checkCollision()) {
+        gameOver = true
+    }
+}
+
+function checkCollision() {
+    return rows.some(row => {
+        return row.cars.some(car => {
+            if (
+                wilbert.x + ROW_HEIGHT >= car.x && 
+                wilbert.x <= (car.x + CAR_WIDTH) &&
+                wilbert.y == car.y
+            ) {
+                return true
+            }
+        })
+    })
+}
+
+let lastVerticalMove = 0
+function canMoveVertically() {
+    return lastVerticalMove < frameCount- 10
+}
+
+let lastHorizontalMove = 0
+function canMoveHorizontally() {
+    return lastHorizontalMove < frameCount- 10
+}
+
+function moveWilbert() {
+    if (keyIsDown(DOWN_ARROW) && canMoveVertically()) {
+        wilbert.y += ROW_HEIGHT
+        lastVerticalMove = frameCount
+    } else if (keyIsDown(UP_ARROW) && canMoveVertically()) {
+        wilbert.y -= ROW_HEIGHT
+        lastVerticalMove = frameCount
+    } else if (keyIsDown(LEFT_ARROW) && canMoveHorizontally()) {
+        wilbert.x -= ROW_HEIGHT
+        lastHorizontalMove = frameCount
+    } else if (keyIsDown(RIGHT_ARROW) && canMoveHorizontally()) {
+        wilbert.x += ROW_HEIGHT
+        lastHorizontalMove = frameCount
+    }
+}
+
+function drawWilbert() {
+    fill("brown")
+    rect(wilbert.x, wilbert.y, ROW_HEIGHT, ROW_HEIGHT)
 }
 
 // A brand new road that starts at the top of the canvas
@@ -78,7 +152,7 @@ function drawRow(row) {
 // Brand new car
 function newCar() {
     return {
-        x: 0,
+        x: random(0, CANVAS_WIDTH),
         y: 0,
         speed: random(.5, 3)
     }
@@ -87,5 +161,5 @@ function newCar() {
 // Draw a car at (x, y)
 function drawCar(car) {
     fill("red")
-    rect(car.x, car.y, 100, ROW_HEIGHT)
+    rect(car.x, car.y, CAR_WIDTH, ROW_HEIGHT)
 }
